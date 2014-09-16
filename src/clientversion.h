e1687407cdd3c0c8a3d85b811acf0c2ad64c011f
Rav3nPL <rav3.pl@gmail.com>
2014-09-16 14:14:32 +0200

    change in .h files

--- src/clientversion.h
+++ src/clientversion.h
@@ -7,7 +7,7 @@
 
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR       0
-#define CLIENT_VERSION_MINOR       12 //doubloons, it is 0873 litecoin in fact
+#define CLIENT_VERSION_MINOR       12 //doubloons, it is 0873 doubloons in fact
 #define CLIENT_VERSION_REVISION    0
 #define CLIENT_VERSION_BUILD       0
 
