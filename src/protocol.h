b54923b2ab2bbab0b4afa3cbba9ac995a9dc752f
io-eightyeight <root@yourself.org>
2014-11-29 22:25:08 -0500

    [Core] Default port changes

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 111337 : 1336; //doubloons
+    return testnet ? 11336 : 1336;
 }
 
 
